05595c069e2b13b252085f558ad0bbce6fb5df8d
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2016-09-05 10:39:09 +0000

    Add excercies form chapter Variables

---
+++ Intro to programming with Ruby/Variable/name.rb
@@ -0,0 +1,20 @@
+#Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included
+
+puts "Type your first name: ";
+first_name = gets.chomp;
+puts "Type your last name: ";
+last_name = gets.chomp;
+
+greeting = "Hello " + first_name +  " " + last_name + "! Nice to see you!";
+
+puts greeting;
+
+=begin
+Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+=end
+name = first_name + " " + last_name;
+10.times do
+  puts name
+end
+
+# Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
